290c78f95d8b48255fa681ddfe8dca2cafd49a9a
Yuna Tomida <ytomida.mmm@gmail.com>
2022-11-05 16:58:53 +0900

    add dylinker

--- lib/machxir/mach_o/load_command/dylinker.ex
+++ lib/machxir/mach_o/load_command/dylinker.ex
@@ -4,10 +4,22 @@ defmodule Machxir.MachO.LoadCommand.Dylinker do
   """
 
   alias Machxir.ByteCrawler
+  alias Machxir.MachO.LcStr
+  alias Machxir.Utils
 
+  @spec parse(pid, :mach | :mach64, :describe | :format) :: [
+          String.t() | list
+        ]
   @doc """
   `pid` must be of the `ByteCrawler`server.
   """
-  def parse(pid) do
+  def parse(pid, arch, _opt) do
+    offset = ByteCrawler.read_uint32(pid)
+    ByteCrawler.read_rawbytes(pid, offset - 12) |> Utils.check_zero_or_empty(__MODULE__)
+    name = LcStr.read_string(pid, arch)
+
+    [
+      "name: #{name} (offset #{offset})"
+    ]
   end
 end
