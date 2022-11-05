93287e7b40ce3292bb87e10b4a9dad18deb9ba73
Yuna Tomida <ytomida.mmm@gmail.com>
2022-11-05 15:46:47 +0900

    add load_command names

--- lib/machxir/mach_o/load_command.ex
+++ lib/machxir/mach_o/load_command.ex
@@ -4,24 +4,29 @@ defmodule Machxir.MachO.LoadCommand do
   alias Machxir.MachO.LoadCommand
   @req_dyld 0x80000000
 
-  @spec parse(any, any, atom, atom) :: [String.t() | list]
+  @spec parse(integer, any, binary, any, :describe | :format) :: [String.t() | list]
   @doc """
-  `opt` must be `:format` or `:describe`
+  `body` must be the content of command **excluding** `cmd` and `cmdsize`.
   """
-  def parse(cmd, bin, endianness, opt = :format) do
-    {:ok, pid} = ByteCrawler.invoke(bin, endianness)
+  def parse(cmd, cmdsize, body, endianness, opt \\ :format) when is_binary(body) do
+    {:ok, pid} = ByteCrawler.invoke(body, endianness)
     type = if (cmd &&& @req_dyld) == 0, do: nil, else: :req_dyld
-    parse_inner(cmd, pid, type, opt)
+
+    [
+      "cmd:     #{format(cmd, type)}",
+      "cmdsize: #{cmdsize}",
+      parse_inner(cmd, cmdsize, pid, type, opt)
+    ]
   end
 
-  defp parse_inner(cmd, pid, nil, opt) do
+  defp parse_inner(cmd, _cmdsize, pid, nil, opt) do
     # TODO: appropriate handling for situation where LC_LEQ_DYLD ones are passed.
     case cmd do
       0x01 -> LoadCommand.Segment.parse(pid, opt)
       0x02 -> LoadCommand.Symtab.parse(pid, opt)
       0x03 -> LoadCommand.Symseg.parse(pid, opt)
       0x04 -> LoadCommand.Thread.parse(pid, opt)
-      0x05 -> LoadCommand.Unixthread.parse(pid, opt)
+      0x05 -> LoadCommand.Thread.Unix.parse(pid, opt)
       0x06 -> LoadCommand.Loadfvmlib.parse(pid, opt)
       0x07 -> LoadCommand.Idfvmlib.parse(pid, opt)
       0x08 -> LoadCommand.Ident.parse(pid, opt)
@@ -40,50 +45,161 @@ defmodule Machxir.MachO.LoadCommand do
       0x15 -> LoadCommand.SubLibrary.parse(pid, opt)
       0x16 -> LoadCommand.TwolevelHints.parse(pid, opt)
       0x17 -> LoadCommand.PrebindCksum.parse(pid, opt)
-      0x18 -> "LoadWeekDylib is LC_REQ_DYLD"
       0x19 -> LoadCommand.Segment64.parse(pid, opt)
       0x1A -> LoadCommand.Routines64.parse(pid, opt)
       0x1B -> LoadCommand.FunctionStarts.parse(pid, opt)
-      0x1C -> "Rpath is LC_REQ_DYLD"
       0x1D -> LoadCommand.CodeSignature.parse(pid, opt)
       0x1E -> LoadCommand.SegmentSplitInfo.parse(pid, opt)
-      0x1F -> "ReexportDylib is LC_REQ_DYLD"
       0x20 -> LoadCommand.LazyLoadDylib.parse(pid, opt)
       0x21 -> LoadCommand.EncryptionInfo.parse(pid, opt)
       0x22 -> LoadCommand.DyldInfo.parse(pid, opt)
-      0x23 -> "LoadUpwardDylib is LC_REQ_DYLD"
-      0x24 -> LoadCommand.VersionMinMacosx.parse(pid, opt)
-      0x25 -> LoadCommand.VersionMinIphoneos.parse(pid, opt)
+      0x24 -> LoadCommand.VersionMin.Macosx.parse(pid, opt)
+      0x25 -> LoadCommand.VersionMin.Iphoneos.parse(pid, opt)
       0x26 -> LoadCommand.FunctionStarts.parse(pid, opt)
       0x27 -> LoadCommand.DyldEnvironment.parse(pid, opt)
-      0x28 -> "Main is LC_REQ_DYLD"
       0x29 -> LoadCommand.DataInCode.parse(pid, opt)
       0x2A -> LoadCommand.SourceVersion.parse(pid, opt)
       0x2B -> LoadCommand.DylibCodeSignDrs.parse(pid, opt)
       0x2C -> LoadCommand.EncryptionInfo64.parse(pid, opt)
       0x2D -> LoadCommand.LinkerOption.parse(pid, opt)
       0x2E -> LoadCommand.LinkerOptimizationHint.parse(pid, opt)
-      0x2F -> LoadCommand.VersionMinTvos.parse(pid, opt)
-      0x30 -> LoadCommand.VersionMinWatchos.parse(pid, opt)
+      0x2F -> LoadCommand.VersionMin.Tvos.parse(pid, opt)
+      0x30 -> LoadCommand.VersionMin.Watchos.parse(pid, opt)
       0x31 -> LoadCommand.Note.parse(pid, opt)
       0x32 -> LoadCommand.BuildVersion.parse(pid, opt)
-      0x33 -> "DyldExportsTrie is LC_REQ_DYLD"
-      0x34 -> "DyldChainedFixups is LC_REQ_DYLD"
-      0x35 -> "FilesetEntry is LC_REQ_DYLD"
+      _ -> []
     end
   end
 
-  defp parse_inner(cmd, pid, :req_dyld, opt) do
+  def format(cmd, nil) do
+    case cmd do
+      0x01 -> "LC_SEGMENT"
+      0x02 -> "LC_SYMTAB"
+      0x03 -> "LC_SYMSEG"
+      0x04 -> "LC_THREAD"
+      0x05 -> "LC_UNIXTHREAD"
+      0x06 -> "LC_LOADFVMLIB"
+      0x07 -> "LC_IDFVMLIB"
+      0x08 -> "LC_IDENT"
+      0x09 -> "LC_FVMFILE"
+      0x0A -> "LC_PREPAGE"
+      0x0B -> "LC_DYSYMTAB"
+      0x0C -> "LC_LOAD_DYLIB"
+      0x0D -> "LC_ID_DYLIB"
+      0x0E -> "LC_LOAD_DYLINKER"
+      0x0F -> "LC_ID_DYLINKER"
+      0x10 -> "LC_PREBOUND_DYLIB"
+      0x11 -> "LC_ROUTINES"
+      0x12 -> "LC_SUB_FRAMEWORK"
+      0x13 -> "LC_SUB_UMBRELLA"
+      0x14 -> "LC_SUB_CLIENT"
+      0x15 -> "LC_SUB_LIBRARY"
+      0x16 -> "LC_TWOLEVEL_HINTS"
+      0x17 -> "LC_PREBIND_CKSUM"
+      0x19 -> "LC_SEGMENT_64"
+      0x1A -> "LC_ROUTINES_64"
+      0x1B -> "LC_UUID"
+      0x1D -> "LC_CODE_SIGNATURE"
+      0x1E -> "LC_SEGMENT_SPLIT_INFO"
+      0x20 -> "LC_LAZY_LOAD_DYLIB"
+      0x21 -> "LC_ENCRYPTION_INFO"
+      0x22 -> "LC_DYLD_INFO"
+      0x24 -> "LC_VERSION_MIN_MACOSX"
+      0x25 -> "LC_VERSION_MIN_IPHONEOS"
+      0x26 -> "LC_FUNCTION_STARTS"
+      0x27 -> "LC_DYLID_ENVIRONMENT"
+      0x29 -> "LC_DATA_IN_CODE"
+      0x2A -> "LC_SOURCE_VERSION"
+      0x2B -> "LC_DYLIB_CODE_SIGN_DRS"
+      0x2C -> "LC_ENCRYPTION_INFO_64"
+      0x2D -> "LC_LINKER_OPTION"
+      0x2E -> "LC_LINKER_OPTIMIZATION_HINT"
+      0x2F -> "LC_VERSION_MIN_TVOS"
+      0x30 -> "LC_VERSION_MIN_WATCHOS"
+      0x31 -> "LC_NOTE"
+      0x32 -> "LC_BUILD_VERSION"
+      _ -> "INVALID (#{cmd})"
+    end
+  end
+
+  defp parse_inner(cmd, _cmdsize, pid, :req_dyld, opt) do
     case cmd &&& bnot(@req_dyld) do
       0x18 -> LoadCommand.LoadWeakDylib.parse(pid, opt)
       0x1C -> LoadCommand.Rpath.parse(pid, opt)
       0x1F -> LoadCommand.ReexportDylib.parse(pid, opt)
-      0x22 -> LoadCommand.DyldInfoOnly.parse(pid, opt)
+      0x22 -> LoadCommand.DyldInfo.Only.parse(pid, opt)
       0x23 -> LoadCommand.LoadUpwardDylib.parse(pid, opt)
       0x28 -> LoadCommand.Main.parse(pid, opt)
       0x33 -> LoadCommand.DyldExportsTrie.parse(pid, opt)
       0x34 -> LoadCommand.DyldChainedFixups.parse(pid, opt)
       0x35 -> LoadCommand.FilesetEntry.parse(pid, opt)
+      _ -> []
+    end
+  end
+
+  def format(cmd, :req_dyld) do
+    case cmd do
+      0x18 -> "LC_WEAK_DYLIB"
+      0x1C -> "LC_RPATH"
+      0x1F -> "LC_REEXPORT_DYLIB"
+      0x22 -> "LC_DYLD_INFO_ONLY"
+      0x23 -> "LC_LOAD_UPWARD_DYLIB"
+      0x28 -> "LC_MAIN"
+      0x33 -> "LC_DYLID_EXPORTS_TRIE"
+      0x34 -> "LC_DYLID_CHAINED_FIXUPS"
+      0x35 -> "FILESET_ENTRY"
+      _ -> "INVALID (#{cmd} found for LC_REQ_DYLD)"
+    end
+  end
+
+  def format(cmd, nil) do
+    case cmd do
+      0x01 -> "LC_SEGMENT"
+      0x02 -> "LC_SYMTAB"
+      0x03 -> "LC_SYMSEG"
+      0x04 -> "LC_THREAD"
+      0x05 -> "LC_UNIXTHREAD"
+      0x06 -> "LC_LOADFVMLIB"
+      0x07 -> "LC_IDFVMLIB"
+      0x08 -> "LC_IDENT"
+      0x09 -> "LC_FVMFILE"
+      0x0A -> "LC_PREPAGE"
+      0x0B -> "LC_DYSYMTAB"
+      0x0C -> "LC_LOAD_DYLIB"
+      0x0D -> "LC_ID_DYLIB"
+      0x0E -> "LC_LOAD_DYLINKER"
+      0x0F -> "LC_ID_DYLINKER"
+      0x10 -> "LC_PREBOUND_DYLIB"
+      0x11 -> "LC_ROUTINES"
+      0x12 -> "LC_SUB_FRAMEWORK"
+      0x13 -> "LC_SUB_UMBRELLA"
+      0x14 -> "LC_SUB_CLIENT"
+      0x15 -> "LC_SUB_LIBRARY"
+      0x16 -> "LC_TWOLEVEL_HINTS"
+      0x17 -> "LC_PREBIND_CKSUM"
+      0x19 -> "LC_SEGMENT_64"
+      0x1A -> "LC_ROUTINES_64"
+      0x1B -> "LC_UUID"
+      0x1D -> "LC_CODE_SIGNATURE"
+      0x1E -> "LC_SEGMENT_SPLIT_INFO"
+      0x20 -> "LC_LAZY_LOAD_DYLIB"
+      0x21 -> "LC_ENCRYPTION_INFO"
+      0x22 -> "LC_DYLD_INFO"
+      0x24 -> "LC_VERSION_MIN_MACOSX"
+      0x25 -> "LC_VERSION_MIN_IPHONEOS"
+      0x26 -> "LC_FUNCTION_STARTS"
+      0x27 -> "LC_DYLID_ENVIRONMENT"
+      0x29 -> "LC_DATA_IN_CODE"
+      0x2A -> "LC_SOURCE_VERSION"
+      0x2B -> "LC_DYLIB_CODE_SIGN_DRS"
+      0x2C -> "LC_ENCRYPTION_INFO_64"
+      0x2D -> "LC_LINKER_OPTION"
+      0x2E -> "LC_LINKER_OPTIMIZATION_HINT"
+      0x2F -> "LC_VERSION_MIN_TVOS"
+      0x30 -> "LC_VERSION_MIN_WATCHOS"
+      0x31 -> "LC_NOTE"
+      0x32 -> "LC_BUILD_VERSION"
+      _ -> "INVALID (#{cmd})"
     end
   end
 end
