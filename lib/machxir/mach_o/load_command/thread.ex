f74d7b90c5ad7cc3bde5e37cb87d8948a0a03b2e
Yuna Tomida <ytomida.mmm@gmail.com>
2022-11-07 17:21:16 +0900

    add thread and fix load_command.ex

--- lib/machxir/mach_o/load_command/thread.ex
+++ lib/machxir/mach_o/load_command/thread.ex
@@ -1,13 +1,30 @@
 defmodule Machxir.MachO.LoadCommand.Thread do
   @moduledoc """
-  Parsing LC_THREAD.
+  Parsing LC_THREAD | LC_UNIXTHREAD.
   """
 
   alias Machxir.ByteCrawler
 
+  @spec parse(pid, integer()) :: [
+          String.t() | list
+        ]
   @doc """
   `pid` must be of the `ByteCrawler`server.
   """
-  def parse(pid) do
+  def parse(pid, cmdsize) do
+    case cmdsize do
+      s when s < 8 ->
+        ["Invalid cmdsize found (#{s})"]
+
+      s when s == 8 ->
+        ["No flavor attatched"]
+
+      _ ->
+        # ignore thread_state
+        [
+          "flavor: #{ByteCrawler.read_int32(pid)}",
+          "count:  #{ByteCrawler.read_int32(pid)}"
+        ]
+    end
   end
 end
