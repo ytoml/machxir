e3622ecf68047f400be9933d6db2973929b21f0b
Yuna Tomida <ytomida.mmm@gmail.com>
2022-11-17 13:36:45 +0900

    add mix task for parsing

---
+++ lib/mix/tasks/parse.ex
@@ -0,0 +1,37 @@
+defmodule Mix.Tasks.Parse do
+  @shortdoc "Parse Mach-O file"
+  use Mix.Task
+
+  def run([]), do: IO.puts("Usage: mix parse [file] [OPTIONS]
+  --verbose, -v   rich output
+  --describe, -d  alias of --verbose
+    ")
+
+  def run(args) do
+    {flags, files, []} =
+      OptionParser.parse(args,
+        strict: [describe: :boolean, verbose: :bool],
+        alias: [d: :describe, v: :verbose]
+      )
+
+    mode = if flags[:describe] or flags[:verbose], do: :describe, else: :format
+
+    case files do
+      [] ->
+        IO.puts(:standard_error, "Error: Please provide filename.")
+
+      [file] ->
+        case File.read(file) do
+          {:ok, binary} ->
+            Machxir.parse(binary, mode)
+            |> IO.puts()
+
+          {:error, posix} ->
+            IO.puts(:standard_error, "Error: Reading file failed(#{inspect(posix)}).")
+        end
+
+      _ ->
+        IO.puts(:standard_error, "Error: Only single file parsing is supported.")
+    end
+  end
+end
